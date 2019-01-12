70768bb5e8adbecc094cd88d1f68d6fdd6e9d5b4
Parag Rao <paragrao@Parags-MacBook-Pro.local>
2019-01-12 12:30:26 -0800

    Pushing my first commit

---
+++ src/test/java/PortnovWebDriver.java
@@ -0,0 +1,13 @@
+public class PortnovWebDriver implements Selenium {
+    public void findElement() {
+        System.out.println("findElement");
+    }
+
+    public void get() {
+        System.out.println("get");
+    }
+
+    public void getTitle() {
+        System.out.println("getTitle");
+    }
+}
